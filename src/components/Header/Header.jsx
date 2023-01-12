25d68a2c686a53be9b6fef088a9b6ab166db30cd
mintmintra <mintracss019@gmail.com>
2023-01-12 18:36:02 +0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <nav>
+            <div></div>
+            <ul>
+                <li><a href="#">ไทย</a></li>
+                <li><a href="#">เข้าสู่ระบบ</a></li>
+            </ul>
+        </nav>
+        <div>
+            <h1>ภาพยนตร์ รายการทีวี และ <br />ความบันเทิงอีกมากมายแบบ <br/>ไม่จำกัด</h1>
+            <h3>รับชมได้ทุกที่ ยกเลิกได้ทุกเมื่อ</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Header
