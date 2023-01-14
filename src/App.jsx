9a53d0916ba7022327695036ea7c302d9cc40313
mintmintra <mintracss019@gmail.com>
2023-01-15 01:14:54 +0700

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-
+import React from "react"
+import Header from "./components/Header/Header"
+import './App.css'
 
 function App() {
 
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
