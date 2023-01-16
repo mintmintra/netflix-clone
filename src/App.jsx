9d52f74cf0d9e37e5016b7a047bd3baac37fd4d5
mintmintra <mintracss019@gmail.com>
2023-01-16 21:36:50 +0700

    Enjoy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./components/Header/Header"
+import Enjoy from "./components/Header/Enjoy/Enjoy"
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Enjoy />
     </div>
   )
 }
