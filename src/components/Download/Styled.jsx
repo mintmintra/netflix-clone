11e55709f95ff51d673de468ce711fd2038b0a41
mintmintra <mintracss019@gmail.com>
2023-01-28 23:46:18 +0700

    Watch

--- src/components/Download/Styled.jsx
+++ src/components/Download/Styled.jsx
@@ -6,4 +6,5 @@ export const DownloadCon = styled.section`
     background: #000;
     padding: 5rem 15rem;
     color: #fff;
+    border-bottom: 8px solid #222;
 `
--- src/components/Watch/Watch.jsx
+++ src/components/Watch/Watch.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { WatchCon } from './Styled'
 
 const Watch = () => {
   return (
-    <div>
+    <WatchCon>
       <div>
-        <h3>รับชมได้ทุกที่</h3>
-        <p>สตรีมภาพยนตร์และรายการทีวีได้ไม่จำกัดใน<br/>โทรศัพท์ แท็บเล็ต แล็ปท็อป และทีวี</p>
+        <h3 style={{ fontSize: '3rem'}}>รับชมได้ทุกที่</h3>
+        <p style={{ fontSize: '1.5rem', marginTop: '1rem'}}>สตรีมภาพยนตร์และรายการทีวีได้ไม่จำกัดใน<br/>โทรศัพท์ แท็บเล็ต แล็ปท็อป และทีวี</p>
       </div>
       <div></div>
-    </div>
+    </WatchCon>
   )
 }
 
