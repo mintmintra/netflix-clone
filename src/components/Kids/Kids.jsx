f04ddc96f67142927d6e042b3e312688914b998d
mintmintra <mintracss019@gmail.com>
2023-02-17 22:11:41 +0700

    Kids

--- src/components/Kids/Kids.jsx
+++ src/components/Kids/Kids.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import kidsImg from "../../assets/kids.png"
+import { KidsCon } from './Styled'
 
 const Kids = () => {
   return (
-    <div>
-        <div>
-            <img src={kidsImg} alt="kidsImg" />
+    <KidsCon>
+        <div style={{ width: '100%'}}>
+            <img width="100%" src={kidsImg} alt="kidsImg" />
         </div>
-        <div>
-            <h3>สร้างโปรไฟล์สำหรับเด็ก</h3>
-            <p>เปิดโลกแห่งการผจญภัยให้บุตรหลานด้วยตัวละคร<br/>โปรดในโปรไฟล์มุมเด็กที่คุณจะใช้งานได้ฟรีเมื่อ<br/>สมัครสมาชิก</p>
+        <div style={{ width: '100%', display: 'flex', alignItems: 'center'}}>
+            <div>
+                <h3 style={{ fontSize: '3rem'}}>สร้างโปรไฟล์สำหรับเด็ก</h3>
+                <p style={{ fontSize: '1.5rem'}}>เปิดโลกแห่งการผจญภัยให้บุตรหลานด้วยตัวละคร<br/>โปรดในโปรไฟล์มุมเด็กที่คุณจะใช้งานได้ฟรีเมื่อ<br/>สมัครสมาชิก</p>
+            </div>
         </div>
-    </div>
+    </KidsCon>
   )
 }
 
